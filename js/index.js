195e4ba0e84eccb96fe1e9f3a57c0dbc8aefcb7b
Eric Masiello <eric.j.masiello@gmail.com>
2018-04-14 21:20:33 -0400

    Solution

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/* global $ */
+
+$(document).ready(function() {
+
+  function readMore(event) {
+    event.preventDefault();
+    $('.readmore').hide();
+    $('#show-this-on-click').slideDown(400, function() {
+      $('.readless').show();
+    });
+  }
+
+  function readLess(event) {
+    event.preventDefault();
+    $('.readless').hide();
+    $('#show-this-on-click').slideUp(400, function() {
+      $('.readmore').show();
+    });
+  }
+
+  function learnMore(event) {
+    event.preventDefault();
+    $(this).hide();
+    $('#learnmoretext').slideDown();
+  }
+
+  $('.readmore a').click(readMore);
+  $('.readless a').click(readLess);
+  $('.learnmore').click(learnMore);
+});
